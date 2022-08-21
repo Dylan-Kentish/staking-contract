bf37c8b6f62d104429cb3c129b32fe95dc40472a
Kent <dylankentish@live.co.uk>
2022-08-21 17:07:17 +0100

    Created deployment script Etherscan verification for contract source code.

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -5,27 +5,47 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const baseURI = "ipfs://QmcVtVtAcFyHtVo3g4DVfjeSYtUymzS7WnvpKJrKNG78DH/"
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+async function deployRewardToken() {
+  const contract = await ethers.getContractFactory("RewardToken")
+  deployed = await contract.deploy();
+  await deployed.deployed()
+  console.log("RewardToken deployed to:", deployed.address)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+async function deployNFT() {
+  const contract = await ethers.getContractFactory("NFT")
+  deployed = await contract.deploy(baseURI);
+  await deployed.deployed()
+  console.log("NFT deployed to:", deployed.address)
+}
+
+async function main() {
+  // await deployRewardToken()
+  // await deployNFT()
+
+  Verify("0xae69A5dD84720B7e1d4C87f78084235Bc075C3E2")
+  Verify("0x7F1739B0f1B0FebD65168885FC6675323AbAf6ab", [baseURI])
+}
+
+async function Verify(address, constructorArguments) {
+  await hre.run("verify:verify", {
+    address: address,
+    constructorArguments: constructorArguments,
+  });
+}
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+// npx hardhat compile
+// npx hardhat run scripts/deploy.js --network goerli
+
+// Latest
+// RewardToken: 0xae69A5dD84720B7e1d4C87f78084235Bc075C3E2
+//              https://goerli.etherscan.io/address/0xae69A5dD84720B7e1d4C87f78084235Bc075C3E2#code
+// NFT:         0x7F1739B0f1B0FebD65168885FC6675323AbAf6ab
+//              https://goerli.etherscan.io/address/0x7F1739B0f1B0FebD65168885FC6675323AbAf6ab#code
